b443c944d42f3fc1bac2b30e5dbd5855c40e7e1f
Apollo <rapollo@protonmail.com>
2021-12-26 23:45:08 +0100

    basic frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>lol</h1>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
