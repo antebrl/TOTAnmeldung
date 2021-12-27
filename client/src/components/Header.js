a1ce8ebafa0e7da029251e448842cfba71bb54b3
Apollo <rapollo@protonmail.com>
2021-12-27 01:46:23 +0100

    "front end"

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "../styles/header.css"
+import logo from "../img/logo.svg"
 
 const Header = () => {
     return(
         <div className="Header">
-            <img src="../img/logo.svg"></img>
+            <div className="header wrapper">
+                <img src={logo} alt="akg" className="logo"></img>
+                <h1>Tag der offenen Tür</h1>
+            </div>
         </div>
     )
 }
