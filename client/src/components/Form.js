8299a10fdad1f54820c245cd38bd57ae73eb08dd
Apollo <rapollo@protonmail.com>
2021-12-29 04:42:00 +0100

    adding rick roll

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -20,6 +20,7 @@ const Form = () => {
                     <Inputs />
                 </div>
             </div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?start=43" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 }
